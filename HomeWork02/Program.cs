684784a2fe6fbec63a62b45bc25c1a8085324fb1
Tatsiana <yasi-anka@bk.ru>
2022-10-21 16:49:01 +0300

    2 задача

--- HomeWork02/Program.cs
+++ HomeWork02/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
-int SecondNumber(int N)
+/*
+int SecondDigit(int N)
 {
    int x = N/10 % 10;
    return x;
@@ -9,13 +9,47 @@ int SecondNumber(int N)
 Console.WriteLine("Введите трёхзначное число ");
 int N = Convert.ToInt32(Console.ReadLine());
 
+if(N < 0)
+N = -1 * N;
+else
 while((N<100)||(N>999))
 {
     Console.WriteLine("Мама роди меня обратно! Введите ТРЁХЗНАЧНОЕ число!!! ");
     int a = Convert.ToInt32(Console.ReadLine());
-    N = a;
+    
+    if(a < 0)
+    N = -1 * a;
 }
 
-int n = SecondNumber(N);
+int n = SecondDigit(N);
 
 Console.WriteLine(n);
+
+*/
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int ThirdDigit(int N)
+{
+    while(N > 999)
+    N = N/10;
+int x = N % 10;
+
+return x;
+}
+
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if( N < 0 )
+    N = -N;
+    
+if( N < 100)
+Console.WriteLine("Третьей цифры нет");
+
+else
+{
+    int n = ThirdDigit(N);
+    Console.WriteLine(n);
+}
+
+
