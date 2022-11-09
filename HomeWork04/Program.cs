7e7ca222661261a5884a687565e5b482837a18f9
Tatsiana <yasi-anka@bk.ru>
2022-11-09 15:04:54 +0300

    Первая задача

---
+++ HomeWork04/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
+
+int Stepen( int a, int b)
+{
+int st = 1;
+
+for (int i=1; i <= b; i++)
+{
+st = st*a;
+}
+return st;
+}
+
+Console.WriteLine("Введите число А ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число В ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("A в степени В равно " + Stepen(A,B));
+
+*/
+
