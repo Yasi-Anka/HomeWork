d08b8b334cff41a36f77a87a453e6a4d7c82a475
Tatsiana <yasi-anka@bk.ru>
2022-11-09 15:40:59 +0300

    Задача 2

--- HomeWork04/Program.cs
+++ HomeWork04/Program.cs
@@ -22,3 +22,23 @@ Console.WriteLine("A в степени В равно " + Stepen(A,B));
 
 */
 
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int FindSum(int N)
+{
+    int Sum = 0;
+    int Ost = 0;
+    while (N >= 10)
+    {
+        Ost = N % 10;
+        Sum = Sum + Ost;
+        N = N/10;
+    }
+    Sum = Sum + N;
+    return Sum;
+}
+
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма цифр равна " + FindSum(N));
