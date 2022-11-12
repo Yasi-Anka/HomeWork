1ccaad21c01474921aca4502d49325acd931f919
Tatsiana <yasi-anka@bk.ru>
2022-11-12 22:49:07 +0300

    Задача 2

--- HomeWork05/Program.cs
+++ HomeWork05/Program.cs
@@ -1,6 +1,4 @@
-﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
-// Напишите программу, которая покажет количество чётных чисел в массиве.
-
+﻿
 int[] CreateRandomeArray(int size, int minValue, int maxValue) //создать рандомный массив величиной size, в диапазорне от ... до ..
 {
     int[] array = new int[size]; // инициализация массива (выделить память для size элементов (заполняется нулями по умолчанию))
@@ -12,13 +10,18 @@ int[] CreateRandomeArray(int size, int minValue, int maxValue) //создать 
         return array;
 }   
 
-void ShowArray(int[] array)
+void ShowArray(int[] array) // Вывести массив на консоль
 {
     for(int i = 0; i < array.Length; i++)
         Console.Write(array[i] + " ");
     
     Console.WriteLine();
 }
+
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+/*
 
 int VolumeEvenArray(int[] array)
 {
@@ -39,4 +42,32 @@ int[] XArray = CreateRandomeArray(n, 100, 1000);
 ShowArray(XArray);
 
 Console.WriteLine("Чётных чисел " + VolumeEvenArray(XArray));
+
+*/
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+
+int SumOddArray(int[] array)
+{
+    int Sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+        Sum = Sum + array[i];
+
+     return Sum;
+}
+
+Console.WriteLine("Введите величину массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное значение элемента массива ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите максимальное значение элемента массива ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] Xarray = CreateRandomeArray(n, min, max );
+ShowArray(Xarray);
+Console.WriteLine("Сумма элементов, стоящих на нечётных позитциях равна " + SumOddArray(Xarray));
 
