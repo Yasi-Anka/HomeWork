464140002b1c7ef36343c0ad20317678011f3133
Tatsiana <yasi-anka@bk.ru>
2022-11-12 16:52:31 +0300

    Задача3

--- HomeWork04/Program.cs
+++ HomeWork04/Program.cs
@@ -23,7 +23,7 @@ Console.WriteLine("A в степени В равно " + Stepen(A,B));
 */
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
+/*
 int FindSum(int N)
 {
     int Sum = 0;
@@ -42,3 +42,38 @@ Console.WriteLine("Введите число ");
 int N = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Сумма цифр равна " + FindSum(N));
+*/
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;//определить длинну массива
+    int index = 0;
+    int x = 0;
+    
+    while (index < length)
+    {
+        Console.WriteLine("Введите элемент массива в ячейку " + index);
+        x = Convert.ToInt32(Console.ReadLine());
+        collection[index] = x;// положить в ячейку с индексом случайно сгенерированное число от 1 до 10
+        index++;
+    }
+
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+int [] array = new int[8];
+
+FillArray(array);
+
+ShowArray(array);
+
+
