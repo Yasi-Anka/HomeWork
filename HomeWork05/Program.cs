576c8b74ac0564f6d3fc3e2d1a40d758d8fc4058
Tatsiana <yasi-anka@bk.ru>
2022-11-14 08:35:13 +0300

    Третья задача

--- HomeWork05/Program.cs
+++ HomeWork05/Program.cs
@@ -47,7 +47,7 @@ Console.WriteLine("Чётных чисел " + VolumeEvenArray(XArray));
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
-
+/*
 
 int SumOddArray(int[] array)
 {
@@ -70,4 +70,65 @@ int max = Convert.ToInt32(Console.ReadLine());
 int[] Xarray = CreateRandomeArray(n, min, max );
 ShowArray(Xarray);
 Console.WriteLine("Сумма элементов, стоящих на нечётных позитциях равна " + SumOddArray(Xarray));
+
+*/
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] CreateRandomeArrayD(int size, double minValue, double maxValue) //создать рандомный массив величиной size, в диапазорне от ... до ..
+{
+    double[] array = new double[size]; // инициализация массива (выделить память для size элементов (заполняется нулями по умолчанию))
+ 
+      for(int i = 0; i < array.Length; i++)
+                        
+            array[i] = (new Random().NextDouble())*(maxValue - minValue) + minValue;
+
+return array;
+}
+
+void ShowArrayD(double[] array) // Вывести массив на консоль
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+double MaxArray(double[] array)
+{
+    double Max = array[0];
+  
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > Max)
+        Max = array[i];
+    }
+    return Max;
+}
+
+double MinArray(double[] array)
+{
+    double Min = array[0];
+  
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < Min)
+        Min = array[i];
+    }
+    return Min;
+}
+
+Console.WriteLine("Введите величину массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите минимальное значение массива ");
+double min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите максимальное значение массива ");
+double max = Convert.ToInt32(Console.ReadLine());
+
+double[] array = CreateRandomeArrayD(n, min, max);
+ShowArrayD(array);
+
+Console.WriteLine("Разница между максимальным и минимальным элементами равна: " + (MaxArray(array) - MinArray(array)));
 
