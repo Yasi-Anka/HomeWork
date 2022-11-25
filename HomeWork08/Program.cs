bd9fb312f4b2bf60f78dd1e31694a5fdd5ee86be
Tatsiana <yasi-anka@bk.ru>
2022-11-25 13:30:37 +0300

    60 задача. БЕЗ проверки на повторяемость

--- HomeWork08/Program.cs
+++ HomeWork08/Program.cs
@@ -71,6 +71,7 @@ Show2dArray(myarray);
 // Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 // ЕСЛИ ОДИНОКОВАЯ СУММА В НЕСКОЛЬКИХ СТРОКАХ?
 
+/*
 int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)//генерация рандомного двумерного массива
 {
     int[,] array = new int[rows, columns];
@@ -144,15 +145,85 @@ Show2dArray(myarray);
 int[] SumArray = SumRows(myarray, m);
 
 Console.WriteLine("Наименьшая сумма элементов в " + Min(SumArray) + " строке (нумерация строк с 0)");
-
+*/
 
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+/*
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)//генерация рандомного двумерного массива
+{
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+                array[i,j] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void Show2dArray(int[,] array)// печать двумерного массива
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + " ");
+
+    Console.WriteLine();
+    }
+}
+*/
 
 
 
-// Задача 58: Задайте две матрицы.\
-// Напишите программу, которая будет находить произведение двух матриц.
+// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу,
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+int[,,] CreateRandom2dArray(int layer1, int layer2, int layer3, int minValue, int maxValue)//генерация рандомного двумерного массива
+{
+    int[,,] array = new int[layer1, layer2, layer3];
+
+    for(int i = 0; i < layer1; i++)
+    {
+        for(int j = 0; j < layer2; j++)
+            {
+                for(int k = 0; k < layer3; k++)
+                array[i,j,k] = new Random().Next(minValue, maxValue + 1);
+            }
+    }
+    return array;
+}
+
+void Show2dArray(int[,,] array)// печать двумерного массива
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(2); k++)
+            Console.Write(array[i,j,k] + "(" + i + "," + j + "," + k + ") ");
+
+        Console.WriteLine();
+        }
+
+     Console.WriteLine();
+    
+    }
+
+}
+
+Console.Write("Введите количество элементов первого уровня массива ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество элементов второго уровня массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество элементов третьего уровня массива ");
+int k = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите минимальный элемент массива ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимакльный элемент массива ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,,] myarray = CreateRandom2dArray(m, n, k, min, max);
+Show2dArray(myarray);
 
 
-
-// Напишите программу, которая заполнит спирально массив 4 на 4.
+// Задача 62: Напишите программу, которая заполнит спирально массив 4 на 4.
